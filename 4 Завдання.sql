bbf9aa75611465eb58e52338ae9da0f00dbf8717
Ihor Tokar <134220707+IhorTokar@users.noreply.github.com>
2024-05-23 23:34:00 +0300

    Update 4 Завдання.sql

--- 4 Завдання.sql
+++ 4 Завдання.sql
@@ -6,6 +6,6 @@ DELETE FROM Users
 WHERE username = 'guest1';
 GO
 
--- Видалення всіх записів з таблиці Roles
-TRUNCATE TABLE Roles;
+-- Видалення всіх записів з таблиці Comments
+DELETE FROM Comments;
 GO
