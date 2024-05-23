c04b9a49d2e9765bdfaa6579d61a5c53c643ba49
Ihor Tokar <134220707+IhorTokar@users.noreply.github.com>
2024-05-23 16:13:41 +0300

    Update and rename pract4.1.sql to 1 Завдання.sql

--- 1 Завдання.sql
+++ 1 Завдання.sql
@@ -1,24 +1,24 @@
 USE CulinaryForum;
 GO
 
--- Одиночне додавання записів
+-- РћРґРёРЅРѕС‡РЅРµ РґРѕРґР°РІР°РЅРЅСЏ Р·Р°РїРёСЃС–РІ
 INSERT INTO Roles (role_name) VALUES ('Admin');
 INSERT INTO Roles (role_name) VALUES ('User');
 INSERT INTO Roles (role_name) VALUES ('Guest');
 GO
 
--- Групове додавання записів
+-- Р“СЂСѓРїРѕРІРµ РґРѕРґР°РІР°РЅРЅСЏ Р·Р°РїРёСЃС–РІ
 INSERT INTO Roles (role_name) VALUES 
 ('Editor'),
 ('Moderator');
 GO
 
--- Одиночне додавання записів
+-- РћРґРёРЅРѕС‡РЅРµ РґРѕРґР°РІР°РЅРЅСЏ Р·Р°РїРёСЃС–РІ
 INSERT INTO Users (username, email, password_hash, role_id) 
 VALUES ('admin', 'admin@example.com', 'hashed_password', 1);
 GO
 
--- Групове додавання записів
+-- Р“СЂСѓРїРѕРІРµ РґРѕРґР°РІР°РЅРЅСЏ Р·Р°РїРёСЃС–РІ
 INSERT INTO Users (username, email, password_hash, role_id)
 VALUES 
 ('user1', 'user1@example.com', 'hashed_password', 2),
